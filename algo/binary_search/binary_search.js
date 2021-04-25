c200a7170decce1c69e4834caa5c8bd73326f6d8
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-25 13:59:57 -0400

    Put binary search algos in their own folder.

--- algo/binary_search/binary_search.js
+++ algo/binary_search/binary_search.js
@@ -1,5 +1,5 @@
 
-const binarySearch = (arr, start_index, max_index, target) => {
+const binarySearchRecursive = (arr, start_index, max_index, target) => {
 
   if (max_index >= start_index){
      
@@ -23,4 +23,31 @@ const binarySearch = (arr, start_index, max_index, target) => {
     return false
   }
   
+}
+
+const binarySearchIterative = (array, target) => {
+
+  let i = 0;
+  let j = array.length-1;
+
+  while(j >= i){
+    let mid = i + (j - i)/2
+    
+    if(array[mid] === target){
+
+      return mid
+
+    }else if(array[mid] > target){
+
+      j = mid - 1
+
+    }else if(array[mid] < target){
+
+      i = mid + 1
+
+    }
+  }
+
+  return -1
+
 }

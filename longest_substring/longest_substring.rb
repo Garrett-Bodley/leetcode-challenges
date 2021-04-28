5609224499161b17708c52ec27efc4fa78e9739b
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-28 01:31:04 -0400

    Include fast method from LeetCode comments.

--- longest_substring/longest_substring.rb
+++ longest_substring/longest_substring.rb
@@ -1,7 +1,8 @@
 # Given a string s, find the length of the longest substring without repeating characters.
 require 'set'
 
-def length_of_longest_substring(s)
+# solution using set
+def length_of_longest_substring_using_set(s)
    
   set = Set.new
   
@@ -20,6 +21,26 @@ def length_of_longest_substring(s)
 
   result
 
+end
+
+# clever solution grabbed from LeetCode comments
+# signficantly faster than solution using set
+
+def length_of_longest_substring(s)
+  stack = []
+  highest_count = 0
+  s.each_char do |c|
+    unless index = stack.index(c)
+      stack.push(c)
+    else
+      stack.push(c)
+      # stack = stack[(index+1)..-1]
+      # you could do the above line, but its much slower. faster to `shift` from array
+      (index + 1).times {stack.shift}
+    end
+    highest_count = stack.length if stack.length > highest_count
+  end
+  highest_count
 end
 
 puts length_of_longest_substring("dvdf")

f22abdf5ee9c5b0c1554bbbe73bedaaaabce1fcb
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-05-18 14:09:22 -0400

    Make K zero indexed, fix return value.

--- longest_palindromic_substring/longest_palindromic_substring.rb
+++ longest_palindromic_substring/longest_palindromic_substring.rb
@@ -33,21 +33,20 @@ def longest_palindromic_substring_dynamic(s)
   #check for 3 letter or greater palindromes
 
   # k is the one indexed length of the substring
-  k = 3
+  k = 2
 
-  while k <= s.length
+  while k < s.length
   
     # iterate through the string
     i = 0
     # subtract one from k to make it a zero indexed length
-    zero_indexed_substring_length = k - 1
-    while i < s.length - zero_indexed_substring_length
+    while i < s.length - k
       # j is zero index of the last character in the current substring 
-      j = i + zero_indexed_substring_length
+      j = i + k
       if s[i] === s[j] &&  table[i + 1][j - 1] == true
         table[i][j] = true
         if k > max
-          max = k
+          max = k + 1
           start = i
         end
       end
@@ -55,10 +54,10 @@ def longest_palindromic_substring_dynamic(s)
     end
     k += 1
   end
-  return s[i...i + max - 1]
+  return s[start...start + max]
 
 end
 
-string = "aabbaabbaabbbbaaaababbaab"
+string = "aaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvvuuuuuuuuuuttttttttttssssssssssrrrrrrrrrrqqqqqqqqqqppppppppppoooooooooonnnnnnnnnnmmmmmmmmmmllllllllllkkkkkkkkkkjjjjjjjjjjiiiiiiiiiihhhhhhhhhhggggggggggffffffffffeeeeeeeeeeddddddddddccccccccccbbbbbbbbbbaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvvuuuuuuuuuuttttttttttssssssssssrrrrrrrrrrqqqqqqqqqqppppppppppoooooooooonnnnnnnnnnmmmmmmmmmmllllllllllkkkkkkkkkkjjjjjjjjjjiiiiiiiiiihhhhhhhhhhggggggggggffffffffffeeeeeeeeeeddddddddddccccccccccbbbbbbbbbbaaaa"
 
 puts longest_palindromic_substring_dynamic(string)

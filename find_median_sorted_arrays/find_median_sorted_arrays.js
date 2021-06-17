8630bd573b0c4853cd6d584373b282744373f215
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-17 16:23:40 -0400

    Solve findMedianSortedArrays using quicksort.

--- find_median_sorted_arrays/find_median_sorted_arrays.js
+++ find_median_sorted_arrays/find_median_sorted_arrays.js
@@ -1,4 +1,25 @@
-const findMedianSortedArrays = function(nums1, nums2) {
-  let [i, j] = 0
+let findMedianSortedArrays = function(nums1, nums2) {
+
+  const merged = quickSort([...nums1, ...nums2])
   
+  return merged.length % 2 === 0 ? 
+    (merged[Math.floor(merged.length/2 -1)] + merged[Math.floor(merged.length/2)])/2
+    : merged[Math.floor(merged.length/2)]
+    
 };
+
+let quickSort = (array) => {
+
+  if(array.length < 2) return array;
+
+  let pivotIndex = Math.floor(Math.random() * array.length)
+  let [front, back] = [[], []];
+
+  array.forEach((el, index) => {
+    if(index === pivotIndex) return;
+    el <= array[pivotIndex] ? front.push(el) : back.push(el)
+  })
+
+  return [...quickSort(front), array[pivotIndex], ...quickSort(back)]
+
+}

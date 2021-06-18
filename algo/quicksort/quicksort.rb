ed0ef2dbc32ae70c782818bf7827da1126e1109b
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-18 12:19:13 -0400

    Clean up syntax and add code comments.

--- algo/quicksort/quicksort.rb
+++ algo/quicksort/quicksort.rb
@@ -1,16 +1,17 @@
 def quick_sort(array)
-
-  return array if array.length < 2
-
+  # Return the array if there is one or zero elements
+  return array if array.length <= 1 
+  # Randomly generate a pivot index and initialize two empty arrays
   pivot = (rand() * (array.length - 1)).round()
-  # binding.pry
-  front = []
-  back = []
+  front, back = [[], []]
+
+  # sort elements in relation to pivot element
   array.each_with_index do |el, index|
+    # Skip pivot element, do not push it to either array
     next if index == pivot
     el <= array[pivot] ? front.push(el) : back.push(el)
   end
-
+  # Recursively sort subarrays and return a flattened, sorted array
   return [quick_sort(front), array[pivot], quick_sort(back)].flatten
 
 end

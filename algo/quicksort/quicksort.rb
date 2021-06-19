ab66deced2b657db64449d3efc468158a6342577
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-19 14:45:49 -0400

    Abstract partitioning out into a helper method.

--- algo/quicksort/quicksort.rb
+++ algo/quicksort/quicksort.rb
@@ -3,16 +3,24 @@ def quick_sort(array)
   return array if array.length <= 1 
   # Randomly generate a pivot index and initialize two empty arrays
   pivot = (rand() * (array.length - 1)).round()
-  front, back = [[], []]
+  # partition array at pivot point
+  front, back = partition(array, pivot)
 
-  # sort elements in relation to pivot element
-  array.each_with_index do |el, index|
-    # Skip pivot element, do not push it to either array
-    next if index == pivot
-    el <= array[pivot] ? front.push(el) : back.push(el)
-  end
   # Recursively sort subarrays and return a flattened, sorted array
   return [quick_sort(front), array[pivot], quick_sort(back)].flatten
 
+end
+
+# partition helper method
+def partition(array, pivot)
+  front, back = [[], []]
+
+  array.each_with_index do |el, index|
+    # Skip the pivot point when partitioning
+    next if index == pivot
+    el <= array[pivot] ? front.push(el) : back.push(el)
+  end
+
+  return [front, back]
 end
 

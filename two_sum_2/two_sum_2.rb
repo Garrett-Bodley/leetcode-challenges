252418748e7f96e0de5011b692c779b1577e5377
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-20 13:19:58 -0400

    Remove binding.pry

--- two_sum_2/two_sum_2.rb
+++ two_sum_2/two_sum_2.rb
@@ -20,7 +20,6 @@ def two_sum(numbers, target)
      
       looking_for = target - num
       answer_2 = binary_search(numbers, index + 1, numbers.length - 1, looking_for)
-      binding.pry
       if !!answer_2
          return [index + 1, answer_2 + 1] 
       end
@@ -32,7 +31,6 @@ def binary_search(array, start_idx, end_idx, target)
   if end_idx >= start_idx
       
     mid = start_idx + (end_idx - start_idx)/2
-    binding.pry
     if array[mid] == target
         return mid
     elsif array[mid] < target

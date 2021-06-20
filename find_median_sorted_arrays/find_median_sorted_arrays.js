139abb415a8060b963806638a97795592956834d
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-19 21:07:00 -0400

    Write solution using two pointers technique.

--- find_median_sorted_arrays/find_median_sorted_arrays.js
+++ find_median_sorted_arrays/find_median_sorted_arrays.js
@@ -1,9 +1,9 @@
-let findMedianSortedArrays = function(nums1, nums2) {
+let findMedianSortedArrays = (nums1, nums2) => {
 
   const merged = quickSort([...nums1, ...nums2])
   
   return merged.length % 2 === 0 ? 
-    (merged[Math.floor(merged.length/2 -1)] + merged[Math.floor(merged.length/2)])/2
+    ( merged[merged.length/2 - 1] + merged[merged.length/2] )/2
     : merged[Math.floor(merged.length/2)]
     
 };
@@ -22,4 +22,38 @@ let quickSort = (array) => {
 
   return [...quickSort(front), array[pivotIndex], ...quickSort(back)]
 
+}
+
+// Faster solution using two pointers
+let findMedianSortedArraysTwoPointers = (nums1, nums2) => {
+
+  let i = 0;
+  let j = 0;
+  let merged = []
+
+  while(i < nums1.length && j < nums2.length){
+    if(nums1[i] < nums2[j]){
+      merged.push(nums1[i])
+      i++
+    }else{
+      merged.push(nums2[j])
+      j++
+    }
+  }
+
+  while(i < nums1.length){
+    merged.push(nums1[i])
+    i++
+  }
+
+  while(j < nums2.length){
+    merged.push(nums2[j])
+    j++
+  }
+
+  if(merged.length % 2 === 0){
+    return ( merged[( merged.length/2 ) - 1] + merged[merged.length/2] )/2
+  }else{
+    return merged[Math.floor(merged.length/2)]
+  }
 }
